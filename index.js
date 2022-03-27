ab59b455b9fdc9fc1e907deafa0022d7d6259a6b
Daniel Gallardo <dgallardo.c@outlook.com>
2022-03-26 19:41:22 -0600

    Remove hover effects with CSS and add them with javascript
    Add helper functions for remove set and clear grid

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const gridContainer = document.querySelector('.sketchArea');
-for (let i = 0; i < 16 * 16; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('gridElement');
-    grid.setAttribute('id', `grid${i}`);
-    gridContainer.appendChild(grid);
+
+function setGridSize(size) {
+    const r = document.querySelector(':root');
+    r.style.setProperty('--total-elements', size);
+
+    for (let i = 0; i < size * size; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('gridElement');
+        grid.setAttribute('id', `grid${i}`);
+        grid.addEventListener("mouseover", onHover);
+        gridContainer.appendChild(grid);
+    }
+}
+
+function clearGrid() {
+    const grid = document.querySelectorAll('.sketchArea>div');
+    grid.forEach(grid => grid.classList.remove('hoveredElement'));
+}
+
+function removeGrid() {
+    gridContainer.innerHTML = '';
+}
+
+function onHover(e) {
+    e.srcElement.classList.add('hoveredElement');
 }
